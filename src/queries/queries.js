b98fe2a03ae3e5a7fc3615064f10dddb3917afca
Punith <kpunith8@gmail.com>
2018-06-22 17:12:13 +0530

    React apollo client

---
+++ src/queries/queries.js
@@ -0,0 +1,27 @@
+import { gql } from 'apollo-boost';
+
+const getBookQuery = gql`
+  {
+    books {
+      name,
+      id,
+    }
+  }`;
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name,
+      id,
+    }
+  }`;
+
+const addBookMutation = gql`
+  mutation($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre:$genre, authorId:$authorId) {
+      name,
+      id,
+    }
+  }`;
+
+export { getBookQuery, getAuthorsQuery, addBookMutation };
